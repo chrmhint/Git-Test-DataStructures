914c8c7b25e1421d8e3e71422f8878e25eceb608
Christina Hinton <chrmhint@ut.utm.edu>
2018-11-27 17:28:37 -0600

    Now main.cpp finds the sum

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n);
+
 int main(){
 
-  cout << "Hello World!" << endl;
+  int n;
+  
+  cout << "Please enter number: ";
+  cin >> n;
 
+  int s = sum(n);
+
+  cout << "The sum is: " << s << endl;
+  
   return 0;
+}
+
+
+int sum(int n){
+
+  int s = 0;
+  for(int i = 1; i <= n; i++)
+    s = s + i;
+
+  return s;
 }
